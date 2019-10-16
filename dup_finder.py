ceaf2eb16424f65957105bcf755528e3475d6966
ipzero209 <npl127.0.0.1@gmail.com>
2019-10-16 14:02:51 -0700

    First push. Most of the bones are here. Still needs a little flesh.

---
+++ dup_finder.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python3
+
+
+import mmh3
+import argparse
+from bitarray import bitarray
+import ruletoken
+import xml.etree.ElementTree as et
+
+
+
+
+
+
+
+def getRules(dg):
+    """Returns a list of security policies from the supplied device group"""
+    pre_rules = dg.findall('pre-rulebase/security/rules/*')
+    post_rules = dg.findall('post-rulebase/security/rules/*')
+    return pre_rules, post_rules
+
+
+def ruleCheck(target, donor):
+    """Builds bloom filter based on rules in target device group and checks for
+    duplicates in the donor ruleset"""
+    # Create and initialize bit array
+    rule_filter = bitarray(10000000)
+    rule_filter.setall(0)
+    # Load rule tokens for target DG into bloom filter
+    target_rule_dict = {}
+    for rule in target:
+        token_dict = ruletoken.genToken(rule)
+        token_list = token_tuple[0]
+        token = "".join(token_list)
+        hash_count = 0
+        while hash_count < 4:
+            hash_result = mmh3.hash(token, hash_count) % 10000000
+            rule_filter[hash_result] = 1
+            hash_count += 1
+    # Check rule tokens for donor DG against bloom filter
+    print(target_rule_dict)
+    for rule in donor:
+        token_list = ruletoken.genToken(rule)
+        token = "".join(token_list)
+        hash_count = 0
+        hash_track = 0
+        while hash_count < 3:
+            hash_result = mmh3.hash(token, hash_count) % 10000000
+            if rule_filter[hash_result] == 0:
+                break
+            else:
+                hash_track += 1
+                hash_count += 1
+        if hash_track == 3:
+            print("{} from the donor DG has a possible match in the target DG.".format(rule.attrib['name']))
+    return
+
+
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--donor', help='Name of the donor device group')
+    parser.add_argument('-t', '--target', help='Name of the target device group')
+    parser.add_argument('-c', '--config', help='Path to the configuration file')
+    args = parser.parse_args()
+
+
+
+
+
+    # Parse XML config
+    config = et.parse(args.config)
+    config_root = config.getroot()
+    dg_list = config_root.findall('./devices/entry/device-group/*')
+    tdg = None
+    ddg = None
+    for dg in dg_list:
+        if dg.attrib['name'] == args.target:
+            print('Target match found!!')
+            tdg = dg
+        elif dg.attrib['name'] == args.donor:
+            print('Donor match found!!')
+            ddg = dg
+    if (tdg or ddg) == None:
+        if tdg == None:
+            print('Error finding target device group. Please check name and '
+                  'try again.')
+            exit(1)
+        else:
+            print('Error finding donor device group. Please check name and '
+                  'try again.')
+            exit(1)
+    # Get lists of Pre and Post rules for both donor and target device groups
+    donor_pre, donor_post = getRules(ddg)
+    target_pre, target_post = getRules(tdg)
+    ruleCheck(target_pre, donor_pre)
+    ruleCheck(target_post, donor_post)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ ruletoken.py
@@ -0,0 +1,44 @@
+def genToken(rule):
+    dest_zones = rule.findall('to/member')
+    d_zone_list = []
+    for zone in dest_zones:
+        d_zone_list.append((zone.text))
+    src_zones = rule.findall('from/member')
+    s_zone_list = []
+    for zone in src_zones:
+        s_zone_list.append(zone.text)
+    src_addrs = rule.findall('source/member')
+    src_addr_list = []
+    for addr in src_addrs:
+        src_addr_list.append(addr.text)
+    dest_addrs = rule.findall('destination/member')
+    dest_addr_list = []
+    for addr in dest_addrs:
+        dest_addr_list.append(addr.text)
+    src_users = rule.findall('source-user/member')
+    src_user_list = []
+    for user in src_users:
+        src_user_list.append(user.text)
+    categories = rule.findall('category/member')
+    cat_list = []
+    for cat in categories:
+        cat_list.append(cat.text)
+    apps = rule.findall('application/member')
+    app_list = []
+    for app in apps:
+        app_list.append(app.text)
+    svcs = rule.findall('service/member')
+    svc_list = []
+    for svc in svcs:
+        svc_list.append(svc.text)
+    hips = rule.findall('hip-profiles/member')
+    hip_list = []
+    for hip in hips:
+        hip_list.append(hip.text)
+    action = rule.find('action').text
+    t_list = d_zone_list + s_zone_list + src_addr_list + dest_addr_list + src_user_list + cat_list + app_list + svc_list + hip_list
+    t_list.append(action)
+    t_list = "".join(t_list)
+    name = rule.attrib['name']
+    token_dict = {t_list : name}
+    return (t_list, name)
